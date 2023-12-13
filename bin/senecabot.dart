6469e765d55b7493d365d95b0f66e7f112da0c40
lovzay <tfvin24@gmail.com>
2023-12-14 06:05:20 +0800

    bot replies when mentioned

---
+++ bin/senecabot.dart
@@ -0,0 +1,20 @@
+import 'package:nyxx/nyxx.dart';
+
+void main() async {
+  final client = await Nyxx.connectGateway(
+    'TOKEN', // Replace this with your bot's token
+    GatewayIntents.allUnprivileged,
+    options: GatewayClientOptions(plugins: [logging, cliIntegration]),
+  );
+
+  final botUser = await client.users.fetchCurrentUser();
+
+  client.onMessageCreate.listen((event) async {
+    if (event.mentions.contains(botUser)) {
+      await event.message.channel.sendMessage(MessageBuilder(
+        content: 'Hi There!',
+        replyId: event.message.id,
+      ));
+    }
+  });
+}
