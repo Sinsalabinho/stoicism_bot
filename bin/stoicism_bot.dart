1a3e820426dad9d6f63dcd3b75b5c5f69870bc2d
lovzay <tfvin24@gmail.com>
2023-12-14 19:08:17 +0800

    combine vars to one

--- bin/stoicism_bot.dart
+++ bin/stoicism_bot.dart
@@ -3,17 +3,15 @@ import 'fetch_quotes.dart';
 
 void main() async {
   final quoteFetcher = StoicQuoteFetcher();
-  var text = await quoteFetcher.fetchQuote();
-  var author = await quoteFetcher.fetchQuote();
-
-  final client =
-      await Nyxx.connectGateway('TOKEN', GatewayIntents.allUnprivileged);
+  var combinedContent = await quoteFetcher.fetchQuote();
+  final client = await Nyxx.connectGateway(
+      'TOKEN', //do not publicize your token
+      GatewayIntents.allUnprivileged);
 
   final botUser = await client.users.fetchCurrentUser();
 
   client.onMessageCreate.listen((event) async {
     if (event.mentions.contains(botUser)) {
-      var combinedContent = '$text\n: $author';
       await event.message.channel.sendMessage(MessageBuilder(
         content: combinedContent,
         replyId: event.message.id,
