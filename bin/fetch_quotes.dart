c8cdc6bbae09d2147ba4d9e5aac5c1ac720961ea
lovzay <tfvin24@gmail.com>
2023-12-14 18:06:08 +0800

    new file for class StoicQuoteFetcher

---
+++ bin/fetch_quotes.dart
@@ -0,0 +1,21 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+class StoicQuoteFetcher {
+  Future<String?> fetchQuote() async {
+    var url = Uri.https('stoic-quotes.com', '/api/quote');
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      var text = jsonResponse['text'];
+      var author = jsonResponse['author'];
+      print('Text: $text');
+      print('\nAuthor: $author.');
+    } else {
+      print('Request failed with status: ${response.statusCode}');
+    }
+    return null;
+  }
+}
