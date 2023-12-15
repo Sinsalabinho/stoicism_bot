6044648377d3cc76fad44c1c447a24e17a98f4a2
lovzay <tfvin24@gmail.com>
2023-12-15 16:57:46 +0800

    fix issue #1

--- bin/stoicism_bot.dart
+++ bin/stoicism_bot.dart
@@ -3,15 +3,15 @@ import 'fetch_quotes.dart';
 
 void main() async {
   final quoteFetcher = StoicQuoteFetcher();
-  var combinedContent = await quoteFetcher.fetchQuote();
   final client = await Nyxx.connectGateway(
-      'TOKEN', //do not publicize your token
+      'Token', //do not publicize your token
       GatewayIntents.allUnprivileged);
 
   final botUser = await client.users.fetchCurrentUser();
 
   client.onMessageCreate.listen((event) async {
     if (event.mentions.contains(botUser)) {
+      var combinedContent = await quoteFetcher.fetchQuote();
       await event.message.channel.sendMessage(MessageBuilder(
         content: combinedContent,
         replyId: event.message.id,
